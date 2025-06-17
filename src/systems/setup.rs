8f718c9bcfb3011ee67e1baaf4f653fa38a76f40
TRIUYEN_TANG <triuyentang@gmail.com>
2025-06-17 14:32:49 +0200

    put back old code

--- src/systems/setup.rs
+++ src/systems/setup.rs
@@ -1,31 +1,10 @@
 use bevy::color::palettes::css::SILVER;
 use bevy::prelude::*;
 
-
 pub fn initialize(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>,
-    ) {
+) {
 
-    // test de Spawn pour vérifier que les cellules sont bien créées
-    // Load and spawn your GLTF model
-    let rock_handle = asset_server.load::<Scene>("tiles/tile_rock.gltf#Scene0");
-    commands.spawn((
-        SceneRoot(rock_handle),
-        Transform::from_xyz(0.0, 0.0, 0.0)
-            .with_scale(Vec3::splat(1.0)),
-    ));
-
-    let leave_anim = asset_server.load::<Scene>("tiles/leaves_animation.gltf#Scene0");
-    commands.spawn((
-        SceneRoot(leave_anim),
-        Transform::from_xyz(0.0, 0.0, 0.0)
-            .with_scale(Vec3::splat(1.0)),
-    ));
-
-    
     commands.spawn((
         PointLight {
             shadows_enabled: true,
