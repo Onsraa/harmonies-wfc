76dd9149f236f8aa254013ead6c5a7c714b51a53
TRIUYEN_TANG <triuyentang@gmail.com>
2025-06-20 12:06:38 +0200

    before pull

--- src/globals.rs
+++ src/globals.rs
@@ -1,5 +1,5 @@
 // Taille par défaut de la grille hexagonale
-pub const DEFAULT_GRID_SIZE: i32 = 100;
+pub const DEFAULT_GRID_SIZE: i32 = 20;
 
 // Hauteur maximale des tuiles (3 niveaux : 0, 1, 2)
 pub const MAX_HEIGHT: u8 = 3;
