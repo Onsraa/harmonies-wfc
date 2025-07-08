49cc7531e61ff26afc531d1b1762d299230cb292
Onsraa <truongteddy306@icloud.com>
2025-07-08 11:04:43 +0200

    optimized avoid collision

--- src/components/spatial.rs
+++ src/components/spatial.rs
@@ -4,4 +4,8 @@ use bevy_spatial::kdtree::KDTree3;
 #[derive(Component, Default)]
 pub struct TrackedByKDTree3D;
 
+#[derive(Component, Default)]
+pub struct ObstacleInKDTree;
+
 pub type NNTree3D = KDTree3<TrackedByKDTree3D>;
+pub type ObstacleTree3D = KDTree3<ObstacleInKDTree>;
